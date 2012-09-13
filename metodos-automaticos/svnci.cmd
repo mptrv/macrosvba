1b384e5d35204e61a1644c84905613b838b08bad
mptrev <mptrev@yahoo.com.br>
2012-09-13 20:27:37 +0000

    Atualizados arquivos 'svnci.cmd' e 'svnup.cmd' para compactarem e descompactarem arquivos que são armazenados no repositório na forma compactada.
    Utiliza-se o 7-Zip (o caminho para '7z.exe' deve estar em %PATH%).

--- metodos-automaticos/svnci.cmd
+++ metodos-automaticos/svnci.cmd
@@ -1,5 +1,5 @@
 @echo off
 echo Realizando 'commit'...
-7z a macros\consulta-sistema-horarios-oficial.7z consulta-sistema-horarios-oficial.accdb
+7z a macros\consulta-sistema-horarios-oficial.7z macros\consulta-sistema-horarios-oficial.accdb
 svn ci -F texto_do_commit.txt
 pause
