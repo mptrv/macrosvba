c318faa34a44ce924b54bff875a7e146100492b5
mptrev@yahoo.com.br <mptrev@yahoo.com.br@5d923ec9-7d1d-cf44-3f95-6bb16d9386be>
2012-11-12 18:23:08 +0000

    SQL:    Atualizações nos /scripts/.

--- trunk/metodos-automaticos/sql/CT-AdicionarEntradaDeVersao.sql
+++ trunk/metodos-automaticos/sql/CT-AdicionarEntradaDeVersao.sql
@@ -1,23 +1,26 @@
 use [sistema-horarios-oficial]
 
+declare @min_sv_th bigint
+declare @min_sv_div bigint
 declare @sv bigint
-declare @min_sv bigint
 
 -- Versão mínima válida.
-set @min_sv = CHANGE_TRACKING_MIN_VALID_VERSION(OBJECT_ID('Tabela_Horarios'));
+set @min_sv_th = CHANGE_TRACKING_MIN_VALID_VERSION(OBJECT_ID('Tabela_Horarios'));
+set @min_sv_div = CHANGE_TRACKING_MIN_VALID_VERSION(OBJECT_ID('Divisao'));
 
 -- Versão corrente.
 set @sv = CHANGE_TRACKING_CURRENT_VERSION();
 
 -- Inserção dos valores na tabela.
-insert into [CT_Versoes] (DataHora, Versao_Minima, Versao_Corrente)
-values (GETDATE(), @min_sv, @sv);
+insert into [CT_Versoes] (DataHora, Versao_Minima_TH, Versao_Minima_Div, Versao_Corrente)
+values (GETDATE(), @min_sv_th, @min_sv_div, @sv);
 
 -- Exibição dos valores recém-inseridos.
 select
 	ctv.ID,
 	ctv.DataHora as 'Data e Hora',
-	ctv.Versao_Minima as 'Versão Mínima Válida',
+	ctv.Versao_Minima_TH as 'Versão Mínima Válida TH',
+	ctv.Versao_Minima_Div as 'Versão Mínima Válida Div',
 	ctv.Versao_Corrente as 'Versão Corrente'	
 from [CT_Versoes] as ctv
 where ctv.ID = (select MAX(ctv.ID) from [CT_Versoes] as ctv);
