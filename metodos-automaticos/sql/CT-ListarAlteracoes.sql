c97fa632f9b2820c6e880d8763b85fe9149ae8fd
mptrev <mptrev@yahoo.com.br>
2012-11-24 16:29:30 +0000

    CSH:
       Implementada a verificação de redução de carga-horária entre anos consecutivos. Este tópico entra no conjunto de restrições.

--- metodos-automaticos/sql/CT-ListarAlteracoes.sql
+++ metodos-automaticos/sql/CT-ListarAlteracoes.sql
@@ -15,7 +15,7 @@ declare @id_tabela int
 set @id_tabela = 0
 
 -- Seleção da versão. Para valores, rodar 'CT-ListarVersoes.sql'.
-set @sv = 129144
+set @sv = 135854
 
 /***** Fim Escolhas do usuário *****/
 
