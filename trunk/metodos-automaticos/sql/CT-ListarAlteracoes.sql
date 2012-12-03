6fc0e77881c02254ea46323ddcb35f3f97eeed5b
mptrev@yahoo.com.br <mptrev@yahoo.com.br@5d923ec9-7d1d-cf44-3f95-6bb16d9386be>
2012-12-03 19:14:32 +0000

    CSH:
       Concluída terceira e quarta etapas para geração de referência cruzada para comparação de quantidades de professores, alunado e recurso humano necessário, com índices.
    
    Solucionados/implementados os itens abaixo, da revisão anterior:
    
       v 3) Gerar total de carga horária por habilitação e concatená-lo às "estatísticas".
    
    
       Próximos passos:
    
    1) Verificar a eficácia dos índices.
    
    2) Criar botão em "Cargas Horárias" para abrir "Consulta Habilitações - Estatísticas".
    
    3) Criar botão em "Cargas Horárias" para abrir "Consulta Habilitações - Os Professores".
    
    4) Gerar relatório gráfico com as informações das "estatísticas".

--- trunk/metodos-automaticos/sql/CT-ListarAlteracoes.sql
+++ trunk/metodos-automaticos/sql/CT-ListarAlteracoes.sql
@@ -15,7 +15,7 @@ declare @id_tabela int
 set @id_tabela = 0
 
 -- Seleção da versão. Para valores, rodar 'CT-ListarVersoes.sql'.
-set @sv = 176691
+set @sv = 177374
 
 /***** Fim Escolhas do usuário *****/
 
