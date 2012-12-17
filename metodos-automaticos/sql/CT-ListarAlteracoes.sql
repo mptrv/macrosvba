6dfd556b29d9602647d9a1fdfae2eadf22a597b8
mptrev <mptrev@yahoo.com.br>
2012-12-17 18:30:55 +0000

    CSH:
       Nas consultas de violação das restrições, todas as tabelas com os resultados agora exibem o RF e o nome formatado do professor.
    
       Próximo passo:
    
         Dar continuação a:
       
         "Em implementação a reetruturação do "Gerenciador Principal", a fim de colocar em evidência as especificações de data e quantidade de hora-aula. Isto, inclusive, se aplicará para as implementações necessárias quanto a alguns dos tópicos abaixo."
       
       Quanto às estatísticas:
    
          Próximos passos:
    
    1) Inverter índices de "... por Aluno".
    
    2) Criar botão em "Cargas Horárias" para abrir "Consulta Habilitações - Estatísticas".
    
    3) Criar botão em "Cargas Horárias" para abrir "Consulta Habilitações - Os Professores".
    
    4) Gerar relatório gráfico com as informações das "estatísticas" ou criar exportação para o Excel.

--- metodos-automaticos/sql/CT-ListarAlteracoes.sql
+++ metodos-automaticos/sql/CT-ListarAlteracoes.sql
@@ -15,7 +15,7 @@ declare @id_tabela int
 set @id_tabela = 0
 
 -- Seleção da versão. Para valores, rodar 'CT-ListarVersoes.sql'.
-set @sv = 190583
+set @sv = 197425
 
 /***** Fim Escolhas do usuário *****/
 
