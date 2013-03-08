664a5981860f335c9a76b20e4dd4db8dd25d009b
mptrev <mptrev@yahoo.com.br>
2013-03-08 00:17:27 +0000

    CGH:
       Acrescentada restrição de acesso às grades de ADMSP e DPs se o modo for o oficial.

--- metodos-automaticos/sql/CT-ListarAlteracoes.sql
+++ metodos-automaticos/sql/CT-ListarAlteracoes.sql
@@ -15,7 +15,7 @@ declare @id_tabela int
 set @id_tabela = 0
 
 -- Seleção da versão. Para valores, rodar 'CT-ListarVersoes.sql'.
-set @sv = 338712
+set @sv = 348423
 
 /***** Fim Escolhas do usuário *****/
 
