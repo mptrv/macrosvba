0e43ab4f40d4cd941ca2b443c544c11a0538d324
mtlorenzetti <mtlorenzetti@gmail.com>
2014-01-30 17:44:38 +0000

    Implementação de consistências no CGH.

--- metodos-automaticos/svnci.cmd
+++ metodos-automaticos/svnci.cmd
@@ -1,5 +1,5 @@
 @echo off
 echo Realizando 'commit'...
-7z u macros\consulta-sistema-horarios-oficial.7z macros\consulta-sistema-horarios-oficial.accdb
+rem 7z u macros\consulta-sistema-horarios-oficial.7z macros\consulta-sistema-horarios-oficial.accdb
 svn ci -F texto_do_commit.txt
 pause
