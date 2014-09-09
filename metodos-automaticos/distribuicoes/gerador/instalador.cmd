97b65a9c4138c2a74d68db63c596caee09902d99
mptrev <mptrev@yahoo.com.br>
2014-09-09 21:16:17 +0000

    MA:    Concluída a elaboração do instalador e do gerador de distribuição.    Lançada a versão o MA 0.0.8.

--- metodos-automaticos/distribuicoes/gerador/instalador.cmd
+++ metodos-automaticos/distribuicoes/gerador/instalador.cmd
@@ -5,43 +5,37 @@ Setlocal
 :: ========== == ======= ===========
 ::
 :: Métodos Automáticos são módulos que fazem parte do Sistema Horários.
+::
+:: TODO: Avaliar: Reescrever todo este 'script' em VBS, junto com
+:: criar-atalhos.vbs?
 
 
 :: Diretórios de origem e destino.
 
 set dir_origem=%~dp0
 
-set dir_dest=%HOMEPATH%\Documentos
-::set dir_dest=E:\Projetos\macrosvba\metodos-automaticos\tmp\instalador\Documentos
-if not exist %dir_dest% set dir_dest=%HOMEPATH%\Documents
-::if not exist %dir_dest% set dir_dest=E:\Projetos\macrosvba\metodos-automaticos\tmp\instalador\Documents
-if not exist %dir_dest% md %dir_dest%
+set dir_dest=%HOMEDRIVE%%HOMEPATH%\Documentos
+if not exist "%dir_dest%" set dir_dest=%HOMEPATH%\Documents
+if not exist "%dir_dest%" md "%dir_dest%"
 
-set dir_dest="%dir_dest%\Metodos Automaticos"
-if not exist %dir_dest% md %dir_dest% &:: Já existente em atualizações.
+set dir_dest=%dir_dest%\Metodos Automaticos
+if not exist "%dir_dest%" md "%dir_dest%" &:: Já existente em atualizações.
 
 :: Cópia dos arquivos para o destino.
 
-robocopy %dir_origem% %dir_dest% * /xf instalador.cmd /xd Documents /xd Documentos /e > nul
+robocopy %dir_origem% "%dir_dest%" * /xf instalador.cmd /xf criar-atalhos.vbs /e > nul
 
 :: Criação de diretórios vazios utilizados pelas aplicações.
 
-if not exist %dir_dest%\relatorios md %dir_dest%\relatorios &:: Já existente em atualizações.
-
-:: Criação dos ícones na área de trabalho.
-:: ...
+if not exist "%dir_dest%\relatorios" md "%dir_dest%\relatorios" &:: Já existente em atualizações.
 
 :: Compartilhamento.
 :: ...
 
+:: Criação dos atalhos na área de trabalho e informação de conclusão.
+ 
+cscript criar-atalhos.vbs //Nologo
+
 :: Finalização.
 
-echo.
-echo INSTALACAO CONCLUIDA!
-echo.
-echo Os atalhos encontram-se na area de trabalho.
-echo.
-pause
-
 Endlocal
-
