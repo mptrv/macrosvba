4024e520bc44d3f1e0e35ec090d4145df0abd61c
mptrev <mptrev@yahoo.com.br>
2014-09-17 02:49:42 +0000

    CGH:    Implementação para atualização automática realizada. Faltam testes finais.

--- metodos-automaticos/distribuicoes/gerador/atualizar.cmd
+++ metodos-automaticos/distribuicoes/gerador/atualizar.cmd
@@ -12,5 +12,23 @@
 :: ser seu fornecimento.
 
 @echo off
+
 if [%1] == [] goto :eof
+
+if not exist \\maua.br\sistemahorarios\metodos-automaticos-%1-a.exe (
+	echo.
+	echo * FALHA NA ATUALIZACAO! *
+	echo.
+	echo O arquivo de atualizacao nao foi encontrado.
+	echo Por favor, entre em contato com o departamento de TI ^(gti@maua.br^).
+	echo.
+	pause
+	goto :eof
+)
+
+:: Espera o fechamento de algum programa.
+:: (Atualmente, devido a atualização a partir de "Construção Grade Horária".)
+timeout 1 /nobreak > nul
+
+:: Dispara a atualização.
 \\maua.br\sistemahorarios\metodos-automaticos-%1-a.exe
