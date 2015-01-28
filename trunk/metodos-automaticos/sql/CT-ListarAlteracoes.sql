e17a4f70940e31b973cc0b61d5aa54d345a785dd
mptrev@yahoo.com.br <mptrev@yahoo.com.br@5d923ec9-7d1d-cf44-3f95-6bb16d9386be>
2015-01-28 20:34:03 +0000

    CGH:
       Os projetos PAE de tutoria sÃ£o disponibilizados para uma determinada sÃ©rie
    de qualquer curso.
    
    CSH:
    
       HÃ¡ consulta temporÃ¡ria para inserir projetos PAE na tabela 'Disciplinas'.
       Criada sala fictÃ­cia SLPAE.
    
    
       [HÃ¡ indÃ­cios de falha com compartilhamento/descompartilhamento de aula entre
    trÃªs turmas, ao se quebrar o compartilhamento de uma e depois de outra (pelo
    'checkbox').]
    
    	[Ainda Falta resolver a questÃ£o da abreviaÃ§Ã£o dos nomes das disciplinas.]

--- trunk/metodos-automaticos/sql/CT-ListarAlteracoes.sql
+++ trunk/metodos-automaticos/sql/CT-ListarAlteracoes.sql
@@ -15,7 +15,7 @@ declare @id_tabela int
 set @id_tabela = 0
 
 -- Seleção da versão. Para valores, rodar 'CT-ListarVersoes.sql'.
-set @sv = 338712
+set @sv = 778178
 
 /***** Fim Escolhas do usuário *****/
 
@@ -40,11 +40,11 @@ select @min_sv as 'Versão Mínima Válida', @atual_sv as 'Versão Corrente', @sv as
 -- Exibição das mudanças.
 
 if @id_tabela = 0 /* Tabela_Horarios */
-	select ct.Id_Turma, ct.Nome_Aula, ct.sys_change_operation, ct.sys_change_columns, ct.sys_change_context
+	select ct.* --, ct.Id_Turma, ct.Nome_Aula, ct.sys_change_operation, ct.sys_change_columns, ct.sys_change_context
 		from changetable(changes Tabela_Horarios, @sv) as ct
 		where ct.sys_change_operation like '%'
-			--and ct.Id_Turma like '%4%D%'
-		order by ct.Id_Turma, Nome_Aula
+			-- and ct.Id_Turma like '%2015%' and ct.sys_change_operation = 'D'
+		order by ct.sys_change_version desc --ct.Id_Turma, Nome_Aula
 else if @id_tabela = 1 /* Divisao */
 	select ct.Id_Turma, ct.sys_change_operation, ct.sys_change_columns, ct.sys_change_context
 		from changetable(changes Divisao, @sv) as ct
