d09d0ce79c3d6b68953b5ef211a524fff4ea7c62
mptrev@yahoo.com.br <mptrev@yahoo.com.br@5d923ec9-7d1d-cf44-3f95-6bb16d9386be>
2015-02-18 21:38:59 +0000

    CGH:
    	Feito ajuste de leiaute da listagem das turmas e corrigido problema de
    estouro quando RF > 32767.
    
    CSH:
    
    	HÃ¡ pequena falha com Disciplinas: mÃºltiplas entradas CurHab para algumas
    delas.
    
       [HÃ¡ indÃ­cios de falha com compartilhamento/descompartilhamento de aula entre
    trÃªs turmas, ao se quebrar o compartilhamento de uma e depois de outra (pelo
    'checkbox').]
    
    	[Ainda Falta resolver a questÃ£o da abreviaÃ§Ã£o dos nomes das disciplinas.]

---
+++ trunk/metodos-automaticos/macros/copia-local.cmd
@@ -0,0 +1,11 @@
+rem 'Script' para atualizar a instala‡Æo local de M‚todos Autom ticos
+rem Nota: este 'script' deve ser salvo com a codifica‡Æo cp850.
+
+@echo off
+echo Copiando construcao-grade_horaria.xlsm...
+copy /y construcao-grade_horaria.xlsm "C:\Users\mptrev\Documents\M‚todos Autom ticos\macros\construcao-grade_horaria_.xlsm"
+echo Copiando consulta-sistema-horarios-oficial.accdb...
+copy /y consulta-sistema-horarios-oficial.accdb "C:\Users\mptrev\Documents\M‚todos Autom ticos\macros\"
+echo Copiando consulta-sistema-horarios-oficial.accdr...
+copy /y consulta-sistema-horarios-oficial.accdr "C:\Users\mptrev\Documents\M‚todos Autom ticos\macros\"
+echo.
